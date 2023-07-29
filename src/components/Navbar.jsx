d905834adddeeef21e9a29eaa6c35d091eb25036
ritumehta <ritumehta281230@gmail.com>
2023-07-29 13:10:26 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link} from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div>Navbar</div>
+
+      <div className="navbar">
+      <Link className="logo-pic" to="App.jsx"><img src=".travel logo.jpg" alt=""/></Link>
+        <div className="travel">
+            <h3>Travel Planner Pro</h3>
+        </div>
+        <div className="menu">
+            <Link to="">Destinations</Link>
+            <Link to="">Hotels</Link>
+            <Link to="">Flights</Link>
+            <Link to="">Bookings</Link>
+            <Link to="">Login</Link>
+            <Link to="">Sign Up</Link>
+        </div>
+      </div>
       <Outlet />
     </>
   );
